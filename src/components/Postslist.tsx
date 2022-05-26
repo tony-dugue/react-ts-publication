e1d8e7b1c82defd06f9eb0c26942cca91857b257
tony dugué <duguetony@gmail.com>
2022-05-26 10:25:41 +0200

    add get posts request & retrieve posts according to the range

---
+++ src/components/Postslist.tsx
@@ -0,0 +1,9 @@
+const Postslist = () => {
+  return (
+    <ul className="Posts">
+
+    </ul>
+  )
+}
+
+export default Postslist
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,14 +1,45 @@
 import './posts.css'
+import Postslist from "../components/Postslist";
+import React, {useEffect, useState} from "react";
 
-const Posts = () => {
+export interface PostData {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const Posts: React.FC = () => {
+
+  const [allPosts, setAllPosts] = useState<PostData[] | null>(null)
+  const [numberOfPosts, setNumberOfPosts] = useState<number>(5)
+
+  useEffect(() => {
+    const getPosts = async() => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${numberOfPosts}`)
+      const data: PostData[] = await response.json()
+      setAllPosts(data)
+    }
+    getPosts()
+  }, [numberOfPosts]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberOfPosts(+e.target.value)
+  }
+
+  console.log(allPosts)
+
   return (
     <div className="post-container">
 
       <h1>Page principale</h1>
 
       <div style={{ display: 'flex', flexDirection: 'column'}}>
-        <label htmlFor="posts">Nombre de publication 5 </label>
-        <input type="range" min={1} max={20} />
+        <label htmlFor="posts">Nombre de publication {numberOfPosts} </label>
+        <input type="range" min={1} max={20} onChange={onChange} />
+
+        <Postslist />
+
       </div>
 
     </div>
