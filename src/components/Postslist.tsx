8b2160850f545f13a73df5af94ef5154e2e3f383
tony dugué <duguetony@gmail.com>
2022-05-26 11:16:43 +0200

    add link & navigation to the detail page

--- src/components/Postslist.tsx
+++ src/components/Postslist.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { PostData } from "../interfaces";
 import './postsList.css';
+import {useNavigate} from "react-router-dom";
 
 interface PostsListProps {
   allPosts: PostData[] | null
 }
 
 const Postslist: React.FC<PostsListProps>  = ({ allPosts }) => {
+
+  const navigate = useNavigate()
+
   return (
     <ul className="posts">
       {allPosts?.map(post => (
-        <li key={post.id}>
+        <li key={post.id} onClick={ () => navigate(`/${post.id}`)}>
           <h2>{post.title}</h2>
           <p>Lire l'article</p>
         </li>
