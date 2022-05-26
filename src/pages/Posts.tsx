d584bed17fb4139be65ccd836275cc1671b39c69
tony dugué <duguetony@gmail.com>
2022-05-26 11:38:44 +0200

    add display range dynamic & memorize number of posts selected

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -9,17 +9,21 @@ const Posts: React.FC = () => {
   const [allPosts, setAllPosts] = useState<PostData[] | null>(null)
   const [numberOfPosts, setNumberOfPosts] = useState<number>(5)
 
+  const verifyNumberOfPosts = () => localStorage.getItem('number') || numberOfPosts;
+  const numberOfPostsSelected = verifyNumberOfPosts()
+
   useEffect(() => {
     const getPosts = async() => {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${numberOfPosts}`)
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${numberOfPostsSelected}`)
       const data: PostData[] = await response.json()
       setAllPosts(data)
     }
     getPosts()
-  }, [numberOfPosts]);
+  }, [numberOfPostsSelected]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNumberOfPosts(+e.target.value)
+    localStorage.setItem('number', e.target.value)
   }
 
   return (
@@ -28,8 +32,8 @@ const Posts: React.FC = () => {
       <h1>Page principale</h1>
 
       <div style={{ display: 'flex', flexDirection: 'column'}}>
-        <label htmlFor="posts">Nombre de publication {numberOfPosts} </label>
-        <input type="range" min={1} max={20} onChange={onChange} />
+        <label htmlFor="posts">Nombre de publication {numberOfPostsSelected} </label>
+        <input type="range" min={1} max={20} onChange={onChange} defaultValue={numberOfPostsSelected} />
 
         <Postslist allPosts={allPosts} />
 
